a310a99d468057ae24bed6f1765f1207c8b47ce7
Rafał Kowalski <rafkow91@gmail.com>
2022-06-08 21:43:43 +0200

    change app to OOP

--- my_secrets.py
+++ my_secrets.py
@@ -1,10 +1,11 @@
+"""File encryptor - console app to encrypt and decrypt files"""
 from argparse import ArgumentParser
-from cryptography.fernet import Fernet
 from getpass import getpass
+
 from os import walk
 from pathlib import Path
 
-from utils import EncryptFile, DecryptFile, AppendToFile, CodeKey
+from utils import Encryption, Decryption
 
 if __name__ == '__main__':
 
@@ -12,13 +13,15 @@ if __name__ == '__main__':
         description='App can encrypt and decrypt files \
             | password is getting on every start app, salt is saved in .env file')
     mode = {
-        'encrypt': EncryptFile,
-        'decrypt': DecryptFile,
-        'append': AppendToFile,
+        'encrypt': Encryption,
+        'decrypt': Decryption,
+        'append': print,
     }
     parser.add_argument(
         '-m', '--mode',
-        help='encrypt given file or files | decrypt encrypted file or files | append -> decrypt file, append text and encrypt the file again',
+        help='encrypt given file or files \
+            | decrypt encrypted file or files \
+            | append -> decrypt file, append text and encrypt the file again',
         choices=mode.keys(),
         default='encrypt'
     )
@@ -41,20 +44,15 @@ if __name__ == '__main__':
     function = mode[args.mode]
     files = args.file
     folder_path = args.dir
+    password =getpass()
 
     if not files:
         files = []
         for path, _, files_in_path in walk(folder_path):
-            try:
-                for file_path in files_in_path:
-                    files.append(f'{path}/{file_path}')
-            except:
-                continue
-            
-    password = getpass()
+            for file_path in files_in_path:
+                files.append(f'{path}/{file_path}')
 
-    my_key = CodeKey(password)
-    fernet = Fernet(my_key)
     for file_path in files:
         path = Path(file_path)
-        function(fernet, path)
+        action = function(password)
+        action.execute(path)
--- utils.py
+++ utils.py
@@ -1,80 +1,29 @@
+"""All classes and functions used in main program"""
 from base64 import urlsafe_b64encode
 from os import listdir
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from dotenv import dotenv_values
+from pathlib import Path
 from random import sample
 from string import ascii_letters
-from cryptography.fernet import Fernet
-from pathlib import Path
+from dotenv import dotenv_values
+
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+from cryptography.fernet import Fernet, InvalidToken
 
 
-def EncryptFile(fernet: Fernet, file_path: Path):
-    if file_path.suffix != '.dokodu':
-        with open(file_path, 'r') as input_file:
-            content = input_file.read()
-
-        encrypted_file = fernet.encrypt(content.encode('utf-8'))
-        if not 'encrypted_files.dokodu' in listdir('./'):
-            open('encrypted_files.dokodu', 'w').close()
-        with open('encrypted_files.dokodu', 'r') as encrypted_files:
-            lines = encrypted_files.readlines()
-
-            for i, line in enumerate(lines):
-                lines[i] = Path(line.strip())
-
-            if file_path not in lines:
-                with open('encrypted_files.dokodu', 'a') as encrypted_files:
-                    encrypted_files.write(f'{file_path}\n')
-
-        with open(file_path.rename(file_path.with_suffix('.dokodu')), 'w') as result:
-            result.write(encrypted_file.decode('utf-8'))
-
-
-def DecryptFile(fernet: Fernet, file_path: Path):
-    
-    if 'encrypted_files.dokodu' in listdir('./'):
-        with open('encrypted_files.dokodu', 'r') as checklist:
-            path = None
-            for line in checklist:
-                path = Path(line.strip())
-                if file_path.with_suffix('.test') == path.with_suffix('.test'):
-                    break
-                else:
-                    path = file_path.with_suffix('.txt')
-    else:
-        path = file_path.with_suffix('.txt')
-
-    with open(file_path, 'r') as input_file:
-        content = input_file.read()
-
-    decrypted_file = fernet.decrypt(content.encode('utf-8'))
-
-    with open(file_path.rename(path), 'w') as result:
-        result.write(decrypted_file.decode('utf-8'))
-
-
-def AppendToFile(fernet: Fernet, file_path: Path):
-    print('File is encrypting')
-
-    print('File is decrypting')
-
-
-def GetSaltFromEnv():
+def get_salt_from_env() -> str:
     while True:
         try:
             my_salt = dotenv_values()['salt']
             break
         except KeyError:
-            with open('.env', mode='a') as settings:
+            with open('.env', mode='a', encoding='utf-8') as settings:
                 settings.write(f'salt="{"".join(sample(ascii_letters, k=10))}"\n')
 
     return my_salt
 
 
-def CodeKey(password):
-    salt = GetSaltFromEnv()
-
+def generate_code_key(password: str, salt: str) -> bytes:
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA512,
         length=32,
@@ -83,3 +32,109 @@ def CodeKey(password):
     )
 
     return urlsafe_b64encode(kdf.derive(bytes(password, 'utf-8')))
+
+
+class _CommonThings:
+    def __init__(self, password: str) -> None:
+        self.password = password
+        self.fernet = self._generate_fernet(self.password)
+
+    @staticmethod
+    def _generate_fernet(password: str, salt: str = None) -> Fernet:
+        if salt is None:
+            salt = get_salt_from_env()
+        key = generate_code_key(password, salt)
+
+        return Fernet(key)
+
+    def execute(self, path: Path) -> None:
+        pass
+
+
+class Encryption(_CommonThings):
+    def __init__(self, password: str) -> None:
+        super().__init__(password)
+
+    def execute(self, path: Path) -> None:
+        self.path = path
+        if self.path.suffix != '.dokodu':
+            try:
+                with open(self.path, 'r', encoding='utf-8') as input_file:
+                    content = input_file.read()
+            except FileNotFoundError:
+                print('This file don\'t exist! You input wrong path')
+                return None
+
+            encrypted_file = self.fernet.encrypt(content.encode('utf-8'))
+            while True:
+                try:
+                    with open('encrypted_files.dokodu', 'r', encoding='utf-8') as encrypted_files:
+                        self.lines = encrypted_files.readlines()
+                        break
+                except FileNotFoundError:
+                    if not 'encrypted_files.dokodu' in listdir('./'):
+                        open('encrypted_files.dokodu', 'w', encoding='utf-8').close()
+
+            try:
+                for i, line in enumerate(self.lines):
+                    self.lines[i] = Path(line.strip())
+            except UnboundLocalError:
+                self.lines = []
+
+            if self.path not in self.lines:
+                with open('encrypted_files.dokodu', 'a', encoding='utf-8') as encrypted_files:
+                    encrypted_files.write(f'{self.path}\n')
+
+            with open(self.path.rename(self.path.with_suffix('.dokodu')), 'w', encoding='utf-8') as result:
+                result.write(encrypted_file.decode('utf-8'))
+
+            print(f'File {path} is encrypted')
+        else:
+            print('This file is not correct suffix - can\'t encrypt file *.dokodu')
+
+
+class Decryption(_CommonThings):
+    def __init__(self, password: str) -> None:
+        super().__init__(password)
+
+    def execute(self, path: Path) -> None:
+        self.path = path
+        try:
+            with open('encrypted_files.dokodu', 'r', encoding='utf-8') as checklist:
+                for line in checklist:
+                    new_path = Path(line.strip())
+                    if self.path.with_suffix('.test') == new_path.with_suffix('.test'):
+                        new_path = Path(line.strip())
+                        break
+                    else:
+                        new_path = self.path.with_suffix('.txt')
+
+        except FileNotFoundError:
+            new_path = self.path.with_suffix('.txt')
+
+        try:
+            type(new_path)
+        except UnboundLocalError:
+            new_path = self.path.with_suffix('.txt')
+
+        try:
+            with open(self.path, 'r') as input_file:
+                content = input_file.read()
+
+            decrypted_file = self.fernet.decrypt(content.encode('utf-8'))
+
+            with open(self.path.rename(new_path), 'w') as result:
+                result.write(decrypted_file.decode('utf-8'))
+
+            print(f'File {path} is decrypted')
+
+        except FileNotFoundError:
+            print('This file don\'t exist! You input wrong path')
+        except InvalidToken:
+            print('This file can\'t be decrypted! You input wrong password')
+
+
+# def AppendToFile(fernet: Fernet, self.path: Path):
+#     print('File is encrypting')
+
+#     print('File is decrypting')
